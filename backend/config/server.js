7bfde072c4692480f6e60d3ebeed426f8d2ac47f
Jongcheol Lee <lee@rrr-z.co.jp>
2021-06-11 12:29:20 +0900

    Create Strapi starter project

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'abeb88953e27733343b9e9f12bf93c76'),
+    },
+  },
+});
