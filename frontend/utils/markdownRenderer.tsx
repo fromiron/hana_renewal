99de8153246d18f718c2901283c8695ce15497f3
fromiron <artless1112@gmail.com>
2021-08-10 17:31:32 +0900

    rehype-raw(5.1.0), remark-gfm(1.0.0)追加でマークアップレンダラーにhtmlタグ表示 各6.0、2.0バージョンはタイプエラー発生FIXされたらアップデート

--- frontend/utils/markdownRenderer.tsx
+++ frontend/utils/markdownRenderer.tsx
@@ -1,5 +1,7 @@
 import ReactMarkdown from 'react-markdown'
 import React from "react";
+import gfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
 
 interface contentType {
     content: string;
@@ -7,14 +9,15 @@ interface contentType {
 
 function MarkdownRenderer({content}: contentType) {
     const API_URL = process.env.NEXT_PUBLIC_API_URL;
-
     //image server addressをapiに変更
     const transformImageUri = (content: string) =>
         /^https?:/.test(content)
             ? content
-            : API_URL+content
+            : API_URL + content
     return (
-        <ReactMarkdown transformImageUri={transformImageUri} children={content}/>
+        <ReactMarkdown transformImageUri={transformImageUri} children={content}
+                       linkTarget={'_blank'} rehypePlugins={[rehypeRaw]} remarkPlugins={[gfm]}
+        />
     )
 }
 
